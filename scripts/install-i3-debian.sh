dd6566634b6a6658c13bafe1a29708289279506d
aodong.qin <ioperations.c@gmail.com>
2021-07-24 07:05:19 +0800

    update script

---
+++ scripts/install-i3-debian.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+ccred="\033[0;31m"
+ccgreen="\033[0;32m"
+ccyellow="\033[0;33m"
+ccwhite="\033[0;37m"
+
+curdir=$(cd `dirname $0` && pwd )
+
+sudo apt install -y hsetroot xsettingsd compton rofi mpc mpv alsa-utils rofi i3-wm  xrandr fcitx dunst -y
+
+if command -v cargo ;then
+    /usr/bin/true
+else
+    source $curdir/rust-tool-chain.sh
+fi
+
+git clone https://github.com/greshake/i3status-rust.git z
+cd z
+cargo install --path .
+./install.sh
--- scripts/install-i3-debina.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-ccred="\033[0;31m"
-ccgreen="\033[0;32m"
-ccyellow="\033[0;33m"
-ccwhite="\033[0;37m"
-sudo apt install rofi i3-wm -y
-cargo install --git https://github.com/greshake/i3status-rust.git
